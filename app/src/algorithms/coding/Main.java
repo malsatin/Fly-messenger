3269aa78789cd189ce09b06c060fac23795e96bd
Denis Chernikov <tchernikoff.denis2013@yandex.ru>
2017-11-06 01:24:32 +0300

    Main in Coding was generalised

--- app/src/algorithms/coding/Main.java
+++ app/src/algorithms/coding/Main.java
@@ -1,30 +1,25 @@
 package algorithms.coding;
 
 import algorithms.exceptions.DecodingException;
+import algorithms.helpers.ByteHelper;
+import algorithms.helpers.NoiseHelper;
 import algorithms.interfaces.ICoder;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-
-import static algorithms.helpers.NoiseHelper.applyNoise;
 
 public class Main {
     private static final double BIT_FLIP_PROB = 0.005;
     private static final String IN_FILE_NAME = "input.txt";
 
     public static void main(String[] args) throws IOException {
-        byte[] file = Files.readAllBytes(Paths.get(IN_FILE_NAME));
+        byte[] file = ByteHelper.readBytesFromFile(IN_FILE_NAME);
         ICoder[] algorithms = {new HammingCode(), new ParityBit(), new RepetitionCode()};
         for (ICoder alg : algorithms) {
             byte[] code = alg.encodeByteString(file);
-            applyNoise(code, BIT_FLIP_PROB);
+            NoiseHelper.applyNoise(code, BIT_FLIP_PROB);
             try {
-                Files.write(Paths.get(alg.getClass().getSimpleName() + "_" + IN_FILE_NAME),
-                        alg.decodeByteString(code),
-                        StandardOpenOption.CREATE,
-                        StandardOpenOption.WRITE);
+                ByteHelper.writeBytesToFile(alg.decodeByteString(code),
+                        alg.getClass().getSimpleName() + "_" + IN_FILE_NAME);
 //                System.out.println(getStringFromBytes(alg.decodeByteString(code)) + '\n');
             } catch (DecodingException e) {
                 e.printStackTrace();
