13292ad1d28a9e38adab18ac96b324dcbba2edd2
Sergey <sergejrf@gmail.com>
2017-11-05 17:07:38 +0300

    Added pseudo-abstract helper

---
+++ app/src/algorithms/helpers/ByteHelper.java
@@ -0,0 +1,29 @@
+package algorithms.helpers;
+
+/**
+ * Fly-messenger
+ * Created by Sergey on 2017-11-05
+ */
+public class ByteHelper {
+
+    public static byte[] getStringBytes(String str) {
+        // TODO
+        return null;
+    }
+
+    public static byte[] getFileBytes(String path) {
+        // TODO
+        return null;
+    }
+
+    public static String bytesToString(byte[] seequence) {
+        // TODO
+        return null;
+    }
+
+    public static void bytesToFile(byte[] seequence, String path) {
+        // TODO
+        return;
+    }
+
+}
