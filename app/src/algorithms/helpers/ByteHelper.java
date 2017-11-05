659c4b640a613a454b6f0a5c1d175ba5a5cd4e3b
Denis Chernikov <tchernikoff.denis2013@yandex.ru>
2017-11-05 21:21:36 +0300

    ByteHelper improved

--- app/src/algorithms/helpers/ByteHelper.java
+++ app/src/algorithms/helpers/ByteHelper.java
@@ -1,29 +1,68 @@
 package algorithms.helpers;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
- * Fly-messenger
- * Created by Sergey on 2017-11-05
+ * This class represents some operations with files as sequences of bytes.
+ * <p>
+ * Created by Sergey Malyutkin on 2017-11-05
  */
 public class ByteHelper {
-
-    public static byte[] getStringBytes(String str) {
-        // TODO
-        return null;
+    /**
+     * Parse a unicode ({@code UTF-8}) string into the sequence of bytes.
+     *
+     * @param str String to convert into an array of bytes
+     * @return Sequence of bytes representing this string
+     */
+    public static byte[] getBytesFromString(String str) {
+        try {
+            return str.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public static byte[] getFileBytes(String path) {
-        // TODO
-        return null;
+    /**
+     * TODO
+     *
+     * @param path
+     * @return
+     * @throws IOException
+     */
+    public static byte[] getBytesFromFile(String path) throws IOException {
+        return Files.readAllBytes(Paths.get(path));
     }
 
-    public static String bytesToString(byte[] seequence) {
-        // TODO
-        return null;
+    /**
+     * TODO
+     *
+     * @param sequence
+     * @return
+     */
+    public static String getStringFromBytes(byte[] sequence) {
+        try {
+            return new String(sequence, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public static void bytesToFile(byte[] seequence, String path) {
-        // TODO
-        return;
+    public static void writeBytesToFile(byte[] sequence, String path) throws FileNotFoundException {
+        PrintWriter pw = new PrintWriter(path);
+        int bufferSize = 1024;
+        int i, j;
+        for (i = 0; i < sequence.length; i += bufferSize) {
+            for (j = 0; j < bufferSize; ++j) {
+                pw.print(sequence[i + j]);
+            }
+            pw.flush();
+        }
     }
-
 }
