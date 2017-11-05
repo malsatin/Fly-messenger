8c03c27c82fbcf2821cb4de0530cafca91e97df0
Sergey <sergejrf@gmail.com>
2017-11-05 16:35:24 +0300

    java test

---
+++ app/src/sources_test/Main.java
@@ -0,0 +1,12 @@
+package sources_test;
+
+/**
+ * Fly-messenger
+ * Created by Sergey on 2017-11-05
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("sdfsdfsdf");
+    }
+}
