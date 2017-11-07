589585801a0972dbfff3d57dee7d1a97b5aa8bb0
Denis Chernikov <tchernikoff.denis2013@yandex.ru>
2017-11-07 21:44:52 +0300

    Small changes in formatting (2)

--- app/src/algorithms/compression/LZMain.java
+++ app/src/algorithms/compression/LZMain.java
@@ -1,14 +1,12 @@
 package algorithms.compression;
 
 /**
- * Example of LZ77 algo usage
- *
- * Created by Sergey on 2017-11-07
+ * Example of LZ77 algorithm usage.
+ * <p>
+ * Created by Sergey Malyutkin on 2017-11-07
  */
 public class LZMain {
-
     public static void main(String[] args) {
-        // Soon there will be some content
+        // TODO
     }
-
 }
--- app/src/algorithms/exceptions/DecodingException.java
+++ app/src/algorithms/exceptions/DecodingException.java
@@ -3,7 +3,7 @@ package algorithms.exceptions;
 /**
  * Exception of an unresolved mistake while decoding.
  * <p>
- * Created by Sergey Malyutkin on 2017-11-05
+ * Created by Denis Chernikov on 2017-11-05
  */
 public class DecodingException extends Exception {
 }
