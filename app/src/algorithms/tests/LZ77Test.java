ba6d9cdf4141259383cbf173969348c408ce8d7d
Sergey <sergejrf@gmail.com>
2017-11-12 20:50:20 +0300

    Moved LZ test

--- app/src/algorithms/tests/LZ77Test.java
+++ app/src/algorithms/tests/LZ77Test.java
@@ -1,11 +1,11 @@
-package algorithms.compression;
+package algorithms.tests;
 
 /**
  * Example of LZ77 algorithm usage.
  * <p>
  * Created by Sergey Malyutkin on 2017-11-07
  */
-public class LZMain {
+public class LZ77Test {
     public static void main(String[] args) {
         // TODO
     }
