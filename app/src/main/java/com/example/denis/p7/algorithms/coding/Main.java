d149be704b6453e8333facfcd445b4b9fc1d7f49
Sergey <sergejrf@gmail.com>
2017-11-14 11:25:14 +0300

    Removed comments from encoding/Main

--- app/src/main/java/com/example/denis/p7/algorithms/coding/Main.java
+++ app/src/main/java/com/example/denis/p7/algorithms/coding/Main.java
@@ -1,29 +1,30 @@
-//package com.example.denis.p7.algorithms.coding;
-//
-//import com.example.denis.p7.algorithms.exceptions.DecodingException;
-//import com.example.denis.p7.algorithms.helpers.ByteHelper;
-//import com.example.denis.p7.algorithms.helpers.NoiseHelper;
-//import com.example.denis.p7.algorithms.interfaces.ICoder;
-//
-//import java.io.IOException;
-//
-//public class Main {
-//    private static final double BIT_FLIP_PROB = 0.005;
-//    private static final String IN_FILE_NAME = "input.txt";
-//
-//    public static void main(String[] args) throws IOException {
-//        byte[] file = ByteHelper.readBytesFromFile(IN_FILE_NAME);
-//        ICoder[] algorithms = {new HammingCode(), new ParityBit(), new RepetitionCode()};
-//        for (ICoder alg : algorithms) {
-//            byte[] code = alg.encodeByteString(file);
-//            NoiseHelper.applyNoise(code, BIT_FLIP_PROB);
-//            try {
-//                ByteHelper.writeBytesToFile(alg.decodeByteString(code),
-//                        alg.getClass().getSimpleName() + "_" + IN_FILE_NAME);
-////                System.out.println(ByteHelper.getStringFromBytes(alg.decodeByteString(code)) + '\n');
-//            } catch (DecodingException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-//}
+package com.example.denis.p7.algorithms.coding;
+
+import com.example.denis.p7.algorithms.exceptions.DecodingException;
+import com.example.denis.p7.algorithms.exceptions.FileTooBigException;
+import com.example.denis.p7.algorithms.helpers.ByteHelper;
+import com.example.denis.p7.algorithms.helpers.NoiseHelper;
+import com.example.denis.p7.algorithms.interfaces.ICoder;
+
+import java.io.IOException;
+
+public class Main {
+    private static final double BIT_FLIP_PROB = 0.005;
+    private static final String IN_FILE_NAME = "input.txt";
+
+    public static void main(String[] args) throws IOException, FileTooBigException {
+        byte[] file = ByteHelper.readBytesFromFile(IN_FILE_NAME);
+        ICoder[] algorithms = {new HammingCode(), new ParityBit(), new RepetitionCode()};
+        for (ICoder alg : algorithms) {
+            byte[] code = alg.encodeByteString(file);
+            NoiseHelper.applyNoise(code, BIT_FLIP_PROB);
+            try {
+                ByteHelper.writeBytesToFile(alg.decodeByteString(code),
+                        alg.getClass().getSimpleName() + "_" + IN_FILE_NAME);
+//                System.out.println(ByteHelper.getStringFromBytes(alg.decodeByteString(code)) + '\n');
+            } catch (DecodingException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
