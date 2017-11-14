473b2818d09b7b716003636f9c65d6788ee27c75
Sergey <sergejrf@gmail.com>
2017-11-14 17:23:32 +0300

    Started work on LZ77

--- app/src/main/java/com/example/denis/p7/algorithms/compression/LZ77.java
+++ app/src/main/java/com/example/denis/p7/algorithms/compression/LZ77.java
@@ -3,6 +3,7 @@ package com.example.denis.p7.algorithms.compression;
 import com.example.denis.p7.algorithms.exceptions.DecompressionException;
 import com.example.denis.p7.algorithms.helpers.BitStream;
 import com.example.denis.p7.algorithms.interfaces.ICompressor;
+import org.apache.commons.compress.utils.BitInputStream;
 
 /**
  * LZ77 compression algorithm
@@ -11,23 +12,48 @@ import com.example.denis.p7.algorithms.interfaces.ICompressor;
  */
 public class LZ77 implements ICompressor {
 
+    /**
+     * Shows, how many bytes we can look back to find such sequence
+     * Optimal to be a multiple of 2
+     */
+    private final int WINDOW_SIZE = 16;
+    /**
+     * Shows, how many bytes can there be in a single sequence
+     * Optimal to be a multiple of 2
+     */
+    private final int FRAME_SIZE = 4;
+
+    private int lookbackBitSize;
+    private int lengthBitSize;
+
+    public LZ77() {
+        lookbackBitSize = convertToBitCount(WINDOW_SIZE);
+        lengthBitSize = convertToBitCount(FRAME_SIZE);
+    }
+
     @Override
     public byte[] compressByteString(byte[] message) {
         BitStream input = new BitStream(message);
         BitStream output = new BitStream();
 
         while(input.hasBits()) {
-            input.readBit();
+            byte currentByte = input.readByte();
+
+            output.addByte(currentByte);
         }
 
         // TODO: implement algorithm
-        return message.clone();
+        return output.toByteArray();
     }
 
     @Override
     public byte[] decompressByteString(byte[] sequence) throws DecompressionException {
         // TODO: implement algorithm
         return sequence.clone();
+    }
+
+    private int convertToBitCount(int length) {
+        return (int)(Math.log(length) / Math.log(2)) + 1;
     }
 
 }
--- app/src/main/java/com/example/denis/p7/algorithms/tests/LZ77Test.java
+++ app/src/main/java/com/example/denis/p7/algorithms/tests/LZ77Test.java
@@ -1,12 +1,25 @@
 package com.example.denis.p7.algorithms.tests;
 
+import com.example.denis.p7.algorithms.compression.LZ77;
+import com.example.denis.p7.algorithms.helpers.ByteHelper;
+
+import java.util.Arrays;
+
 /**
  * Example of LZ77 algorithm usage.
  * <p>
  * Created by Sergey Malyutkin on 2017-11-07
  */
 public class LZ77Test {
+
     public static void main(String[] args) {
-        // TODO
+        LZ77 algo = new LZ77();
+
+        byte[] in = ByteHelper.getBytesFromString("testй");
+        byte[] out = algo.compressByteString(in);
+
+        System.out.println(Arrays.toString(out) + " " + ByteHelper.getStringFromBytes(out));
+        System.out.println("Input size: " + in.length + "; output size: " + out.length);
     }
+
 }
