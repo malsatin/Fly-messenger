d2d3966d493c4cda31d01690f11bb4c883b409b6
mshvdkmv <evmiev@icloud.com>
2017-11-15 19:58:23 +0300

    repet

--- app/src/main/java/com/example/denis/p7/algorithms/coding/RepetitionCode.java
+++ app/src/main/java/com/example/denis/p7/algorithms/coding/RepetitionCode.java
@@ -3,24 +3,35 @@ package com.example.denis.p7.algorithms.coding;
 import com.example.denis.p7.algorithms.exceptions.DecodingException;
 import com.example.denis.p7.algorithms.interfaces.ICoder;
 
+import java.util.BitSet;
 import java.util.LinkedList;
 
 public class RepetitionCode implements ICoder {
     @Override
     public byte[] encodeByteString(byte[] message) {
-        LinkedList<Byte> list = new LinkedList<>();
-        for (byte b : message) {  // TODO replace with algorithm
-            list.add(b);
+        BitSet set = BitSet.valueOf(message);
+        BitSet answer = new BitSet();
+        int index = 0;
+        for (int i = 0; i < set.length(); i++) {
+            boolean temp = set.get(i);
+            for (int k = 0; k < 3; k++) {
+                answer.set(index++, temp);
+            }
         }
-        return getArray(list);
+        return answer.toByteArray();
     }
 
     @Override
     public byte[] decodeByteString(byte[] sequence) throws DecodingException {
-        LinkedList<Byte> list = new LinkedList<>();
-        for (byte b : sequence) {  // TODO replace with algorithm
-            list.add(b);
+        int index = 0;
+        BitSet set = BitSet.valueOf(sequence);
+        BitSet answer = new BitSet();
+        for (int i = 0; i <sequence.length();i++){
+            if(i%3==0){
+                if (answer.get(i)&answer.get(i+1)|answer.get(i+1)&answer.get(i+2)) answer.set(index++,true);
+                else set.set(index++,false);
+            }
         }
-        return getArray(list);
+        return answer.toByteArray();
     }
 }
