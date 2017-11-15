8a37853b5ac558b96e6239d99cdaf19a5a8b7932
DenisRang <32134146+DenisRang@users.noreply.github.com>
2017-11-15 20:02:38 +0300

    added AsyncTasks

--- app/src/main/java/com/example/denis/p7/algorithms/coding/RepetitionCode.java
+++ app/src/main/java/com/example/denis/p7/algorithms/coding/RepetitionCode.java
@@ -1,12 +1,15 @@
 package com.example.denis.p7.algorithms.coding;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
 import com.example.denis.p7.algorithms.exceptions.DecodingException;
 import com.example.denis.p7.algorithms.interfaces.ICoder;
 
 import java.util.BitSet;
-import java.util.LinkedList;
 
 public class RepetitionCode implements ICoder {
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public byte[] encodeByteString(byte[] message) {
         BitSet set = BitSet.valueOf(message);
@@ -21,12 +24,13 @@ public class RepetitionCode implements ICoder {
         return answer.toByteArray();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public byte[] decodeByteString(byte[] sequence) throws DecodingException {
         int index = 0;
         BitSet set = BitSet.valueOf(sequence);
         BitSet answer = new BitSet();
-        for (int i = 0; i <sequence.length();i++){
+        for (int i = 0; i <sequence.length;i++){
             if(i%3==0){
                 if (answer.get(i)&answer.get(i+1)|answer.get(i+1)&answer.get(i+2)) answer.set(index++,true);
                 else set.set(index++,false);
