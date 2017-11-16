a0c3facde6828ac1c304fa43f1b58076df4410d7
Kirdyasheva <kirdyasheva98@gmail.com>
2017-11-16 15:32:57 +0300

    Update Huffman.java

--- app/src/main/java/com/example/denis/p7/algorithms/compression/Huffman.java
+++ app/src/main/java/com/example/denis/p7/algorithms/compression/Huffman.java
@@ -11,7 +11,6 @@ public class Huffman {
     public byte[] compressByteString(byte[] message) {
         this.size = message.length;
         Map<Byte, Integer> map = countFrequency(message);
-        stream = serializeMessage(map);
         this.root = buildTree(map);
         Map<Byte, String> codes = new HashMap<>();
         if (root.isLeaf()) {
@@ -20,6 +19,7 @@ public class Huffman {
             generateCode(root, codes, "");
         }
         byte[] encoded = encodeMessage(codes, message);
+        stream = serializeMessage(encoded, map);
         return encoded;
     }
 
@@ -28,13 +28,15 @@ public class Huffman {
         return stream;
     }
 
-    public byte[] decompressByteString(byte[] sequence, BitStream inStream) {
-        Map<Byte, Integer> map = deserializeMessage(inStream);
+    public byte[] decompressByteString(BitStream inStream) {
+        Map<Byte, Integer> map = deserializeMap(inStream);
+        int size = inStream.readInt();
+        byte[] sequence = deserializeMessage(inStream);
         Node root = buildTree(map);
         byte[] message = new byte[size];
         BitStream stream = new BitStream(sequence);
         int i = 0;
-        while (!stream.isEmpty() && i < size) {
+        while (i < size) {
             Node current = root;
             while (current.left != null) {
                 if (!stream.readBit()) {
@@ -99,22 +101,36 @@ public class Huffman {
         return encoded.toByteArray();
     }
 
-    private BitStream serializeMessage(Map<Byte, Integer> map) {
+    private BitStream serializeMessage(byte[] message, Map<Byte, Integer> map) {
         BitStream stream = new BitStream();
         stream.addInt(map.size());
         for (Map.Entry<Byte, Integer> entry : map.entrySet()) {
             stream.addByte(entry.getKey());
             stream.addInt(entry.getValue());
         }
+        stream.addInt(size);
+        stream.addInt(message.length);
+        for (int i = 0; i < message.length; i++) {
+            stream.addByte(message[i]);
+        }
         return stream;
     }
 
-    private Map<Byte, Integer> deserializeMessage(BitStream stream) {
+    private Map<Byte, Integer> deserializeMap(BitStream stream) {
         Map<Byte, Integer> map = new HashMap<>();
         int size = stream.readInt();
         for (int i = 0; i < size; i++) {
             map.put(stream.readByte(), stream.readInt());
         }
         return map;
+    }
+
+    private byte[] deserializeMessage(BitStream stream) {
+        int size = stream.readInt();
+        byte[] message = new byte[size];
+        for (int i = 0; i < size; i++) {
+            message[i] = stream.readByte();
+        }
+        return message;
     }
 }
