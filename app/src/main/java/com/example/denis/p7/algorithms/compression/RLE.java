3189f39548da60347d33f4d9b9a298a31618d026
Sergey <sergejrf@gmail.com>
2017-11-17 18:36:06 +0300

    RLE placeholder improved

--- app/src/main/java/com/example/denis/p7/algorithms/compression/RLE.java
+++ app/src/main/java/com/example/denis/p7/algorithms/compression/RLE.java
@@ -1,6 +1,7 @@
 package com.example.denis.p7.algorithms.compression;
 
 import com.example.denis.p7.algorithms.exceptions.DecompressionException;
+import com.example.denis.p7.algorithms.helpers.BitStream;
 import com.example.denis.p7.algorithms.interfaces.ICompressor;
 
 /**
@@ -10,16 +11,30 @@ import com.example.denis.p7.algorithms.interfaces.ICompressor;
  */
 public class RLE implements ICompressor {
 
+    /**
+     * Shows, how many bytes can there be in a single sequence
+     * Optimal to be a (power of 2) - 1. About 7-15 is a good variant
+     */
+    private final int FRAME_SIZE = 7;
+
     @Override
     public byte[] compressByteString(byte[] message) {
+        BitStream inStream = new BitStream(message);
+        BitStream outStream = new BitStream();
+
         // TODO: implement algorithm
-        return message.clone();
+
+        return outStream.toByteArray();
     }
 
     @Override
     public byte[] decompressByteString(byte[] sequence) throws DecompressionException {
+        BitStream inStream = new BitStream(sequence);
+        BitStream outStream = new BitStream();
+
         // TODO: implement algorithm
-        return sequence.clone();
+
+        return outStream.toByteArray();
     }
 
 }
