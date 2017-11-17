31509e9ac6b4af7514577659d0c0a0125043b9bf
Denis Chernikov <tchernikoff.denis2013@yandex.ru>
2017-11-18 02:39:13 +0300

    HammingCode - encoding added

--- app/src/main/java/com/example/denis/p7/algorithms/coding/HammingCode.java
+++ app/src/main/java/com/example/denis/p7/algorithms/coding/HammingCode.java
@@ -1,26 +1,45 @@
 package com.example.denis.p7.algorithms.coding;
 
 import com.example.denis.p7.algorithms.exceptions.DecodingException;
+import com.example.denis.p7.algorithms.helpers.BitStream;
 import com.example.denis.p7.algorithms.interfaces.ICoder;
 
-import java.util.LinkedList;
-
 public class HammingCode implements ICoder {
+    private static final int BITS_OF_DATA = 4;
+    private static final int PARITY_BITS = 3;
+
     @Override
     public byte[] encodeByteString(byte[] message) {
-        LinkedList<Byte> list = new LinkedList<>();
-        for (byte b : message) {  // TODO replace with algorithm
-            list.add(b);
+        BitStream in = new BitStream(message);
+        BitStream out = new BitStream();
+        BitStream parityBits = new BitStream();
+        long[] data = new long[BITS_OF_DATA];
+        long i;
+        int j;
+        for (i = 1; i < in.size(); i += BITS_OF_DATA) {
+            for (j = 0; j < BITS_OF_DATA; ++j) {
+                data[j] = in.readBit() ? 1L : 0L;
+                out.addBit(data[j] == 1);
+            }
+            /* Attention! Constant block of code. To be refactored if any of constants above constant will change! */
+            parityBits.addBit((data[0] ^ data[1] ^ data[3]) == 1);
+            parityBits.addBit((data[0] ^ data[2] ^ data[3]) == 1);
+            parityBits.addBit((data[1] ^ data[2] ^ data[3]) == 1);
+            /* End of constant block of code */
         }
-        return getArray(list);
+        out.addByteArray(parityBits.toByteArray());
+        return out.toByteArray();
     }
 
     @Override
     public byte[] decodeByteString(byte[] sequence) throws DecodingException {
-        LinkedList<Byte> list = new LinkedList<>();
-        for (byte b : sequence) {  // TODO replace with algorithm
-            list.add(b);
+        BitStream in = new BitStream(sequence);
+        BitStream out = new BitStream();
+        long i;
+        for (i = 0; i < in.size() * BITS_OF_DATA / (BITS_OF_DATA + PARITY_BITS) - BITS_OF_DATA - PARITY_BITS; i += BitStream.BYTE_SIZE) {
+            out.addByte(in.readByte());  // Wrong! Check on the way...
         }
-        return getArray(list);
+        //in.size() - (BitStream.BYTE_SIZE / BITS_OF_DATA) * in.size() % BITS_OF_DATA; i += BITS_OF_DATA
+        return out.toByteArray();
     }
 }
--- app/src/main/java/com/example/denis/p7/algorithms/coding/ParityBit.java
+++ app/src/main/java/com/example/denis/p7/algorithms/coding/ParityBit.java
@@ -6,14 +6,13 @@ import com.example.denis.p7.algorithms.interfaces.ICoder;
 
 public class ParityBit implements ICoder {
     private static final int BITS_OF_DATA = 8;
-    private static final int BITS_ENCODED = BITS_OF_DATA + 1;
+    private static final int BIT_PORTIONS = BITS_OF_DATA + 1;
 
     @Override
     public byte[] encodeByteString(byte[] message) {
         BitStream in = new BitStream(message);
         BitStream out = new BitStream();
-        long i, j;
-        long cur, control;
+        long i, j, cur, control;
         for (i = 0; i < in.size(); i += BITS_OF_DATA) {
             cur = in.readNumber(BITS_OF_DATA);
             out.addNumber(cur, BITS_OF_DATA);
@@ -30,12 +29,11 @@ public class ParityBit implements ICoder {
     public byte[] decodeByteString(byte[] sequence) throws DecodingException {
         BitStream in = new BitStream(sequence);
         BitStream out = new BitStream();
-        long i, j;
-        long cur, control;
-        for (i = 0; i < in.size() - BITS_OF_DATA; i += BITS_ENCODED) {
-            cur = in.readNumber(BITS_ENCODED);
+        long i, j, cur, control;
+        for (i = 0; i < in.size() - BITS_OF_DATA; i += BIT_PORTIONS) {
+            cur = in.readNumber(BIT_PORTIONS);
             control = 0;
-            for (j = 0; j < BITS_ENCODED; j++) {
+            for (j = 0; j < BIT_PORTIONS; j++) {
                 control ^= (cur & (1L << j)) >>> j;
             }
             System.out.println();
